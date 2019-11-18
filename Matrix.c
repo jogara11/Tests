c8640f8522733916cc0400a9715e705e89529028
jogara11 <jogara11@users.noreply.github.com>
2019-11-18 12:39:23 -0600

    Create Matrix.c

---
+++ Matrix.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct{
+    int rows;
+    int columns;
+    int ** data;
+} sMatrix;
+
+
+void vDestroyMatrix(sMatrix * matrix)
+{
+    free((matrix->data));
+    free(matrix);
+}
+
+
+sMatrix * iptrCreateMatrix(int rows, int columns)
+{
+    sMatrix * sRetMatrix;
+    int ** intDataMatrix;
+    sRetMatrix = (sMatrix *)malloc(sizeof(sMatrix));
+    (sRetMatrix->rows) = rows;
+    (sRetMatrix->columns) = columns;
+
+    (sRetMatrix->data) = (int **) (malloc(rows * sizeof(int *)));
+    for (int i = 0; i < rows; ++i)
+    {
+        (sRetMatrix->data)[i] = (int *)malloc(columns * sizeof(int));
+    }
+
+    return sRetMatrix;
+}
+
+
+void vInitMatrix(sMatrix * matrix)
+{
+    for(int i=0; i<(matrix->rows); i++)
+    {
+        for(int j=0; j<(matrix->columns); j++)
+        {
+            (matrix->data)[i][j] = 0;
+        }
+    }
+}
+
+
+void vPrintMatrix(sMatrix * matrix)
+{
+    printf("\n");
+    for(int i=0; i<(matrix->rows); i++)
+    {
+        for(int j=0; j<(matrix->columns); j++)
+        {
+            printf("%d\t",(matrix->data)[i][j]);
+        }
+        printf("\n");
+    }
+}
